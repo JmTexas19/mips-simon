2de510365fcb958ab95ffed7a49012df2de5bdb3
DazedFury <jmtexas19@gmail.com>
2018-11-11 13:56:23 -0500

    feat: Add digits to circles

--- mips-simon.asm
+++ mips-simon.asm
@@ -21,6 +21,10 @@
 	introPrompt:		.asciiz "Welcome to Simon Says! Enter 1 for easy, 2 for normal, 3 for hard. Enter 0 to quit."
 	colourPrompt:		.asciiz "BLUE: 1\nGREEN: 2\nRED: 3\nMAGENTA: 4\n"
 	invalidNumPrompt:	.asciiz "Invalid number entered, please try again."
+	digit1:			.asciiz "1"
+	digit2:			.asciiz "2"
+	digit3:			.asciiz "3"
+	digit4:			.asciiz "4"
 	
 	#COLOR TABLE
 	colourTable:		.word	0x000000	#Black
@@ -31,6 +35,35 @@
 				.word	0xff00ff	#Blue-Red
 				.word	0xffff00	#Green-Red
 				.word	0xffffff	#White
+			
+	#DIGIT TABLE	
+	DigitTable:
+        .byte   ' ', 0,0,0,0,0,0,0,0,0,0,0,0
+        .byte   '0', 0x7e,0xff,0xc3,0xc3,0xc3,0xc3,0xc3,0xc3,0xc3,0xc3,0xff,0x7e
+        .byte   '1', 0x38,0x78,0xf8,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18,0x18
+        .byte   '2', 0x7e,0xff,0x83,0x06,0x0c,0x18,0x30,0x60,0xc0,0xc1,0xff,0x7e
+        .byte   '3', 0x7e,0xff,0x83,0x03,0x03,0x1e,0x1e,0x03,0x03,0x83,0xff,0x7e
+        .byte   '4', 0xc3,0xc3,0xc3,0xc3,0xc3,0xff,0x7f,0x03,0x03,0x03,0x03,0x03
+        .byte   '5', 0xff,0xff,0xc0,0xc0,0xc0,0xfe,0x7f,0x03,0x03,0x83,0xff,0x7f
+        .byte   '6', 0xc0,0xc0,0xc0,0xc0,0xc0,0xfe,0xfe,0xc3,0xc3,0xc3,0xff,0x7e
+        .byte   '7', 0x7e,0xff,0x03,0x06,0x06,0x0c,0x0c,0x18,0x18,0x30,0x30,0x60
+        .byte   '8', 0x7e,0xff,0xc3,0xc3,0xc3,0x7e,0x7e,0xc3,0xc3,0xc3,0xff,0x7e
+        .byte   '9', 0x7e,0xff,0xc3,0xc3,0xc3,0x7f,0x7f,0x03,0x03,0x03,0x03,0x03
+        .byte   '+', 0x00,0x00,0x00,0x18,0x18,0x7e,0x7e,0x18,0x18,0x00,0x00,0x00
+        .byte   '-', 0x00,0x00,0x00,0x00,0x00,0x7e,0x7e,0x00,0x00,0x00,0x00,0x00
+        .byte   '*', 0x00,0x00,0x00,0x66,0x3c,0x18,0x18,0x3c,0x66,0x00,0x00,0x00
+        .byte   '/', 0x00,0x00,0x18,0x18,0x00,0x7e,0x7e,0x00,0x18,0x18,0x00,0x00
+        .byte   '=', 0x00,0x00,0x00,0x00,0x7e,0x00,0x7e,0x00,0x00,0x00,0x00,0x00
+        .byte   'A', 0x18,0x3c,0x66,0xc3,0xc3,0xc3,0xff,0xff,0xc3,0xc3,0xc3,0xc3
+        .byte   'B', 0xfc,0xfe,0xc3,0xc3,0xc3,0xfe,0xfe,0xc3,0xc3,0xc3,0xfe,0xfc
+        .byte   'C', 0x7e,0xff,0xc1,0xc0,0xc0,0xc0,0xc0,0xc0,0xc0,0xc1,0xff,0x7e
+        .byte   'D', 0xfc,0xfe,0xc3,0xc3,0xc3,0xc3,0xc3,0xc3,0xc3,0xc3,0xfe,0xfc
+        .byte   'E', 0xff,0xff,0xc0,0xc0,0xc0,0xfe,0xfe,0xc0,0xc0,0xc0,0xff,0xff
+        .byte   'F', 0xff,0xff,0xc0,0xc0,0xc0,0xfe,0xfe,0xc0,0xc0,0xc0,0xc0,0xc0
+# add additional characters here....
+# first byte is the ascii character
+# next 12 bytes are the pixels that are "on" for each of the 12 lines
+        .byte    0, 0,0,0,0,0,0,0,0,0,0,0,0
 	
 .text
 main:
@@ -38,14 +71,20 @@ main:
 	la		$sp, stack_end
 
 	#DRAW BLUE CIRCLE
-	la		$a0, 64			#x = 1
-	la		$a1, 64			#y = 1
-	la		$a2, 1			#colour = 1
-	la		$a3, 64			#square size = 14
+	la		$a0, 64			#x 
+	la		$a1, 64			#y 
+	la		$a2, 1			#colour
+	la		$a3, 64			#square size 
 	blueFillLoop:
 	jal		drawCircle		#Jump and link to drawCircle
 	addi		$a3, $a3, -1		#Decrement radius
 	bnez		$a3, blueFillLoop	#If a3 is not 0, branch
+	
+	#DRAW DIGIT
+	li		$a0, 59			#x 
+	li		$a1, 59			#y 
+	la		$a2, digit1
+	jal		OutText
 	
 	#PLAY TONE BLUE
 	li		$a0, 60			#Pitch
@@ -56,14 +95,20 @@ main:
 	syscall					#Execute
 	
 	#DRAW GREEN CIRCLE
-	la		$a0, 192		#x = 1
-	la		$a1, 64			#y = 1
-	la		$a2, 2			#colour = 1
-	la		$a3, 64			#square size = 14
+	la		$a0, 192		#x 
+	la		$a1, 64			#y 
+	la		$a2, 2			#colour 
+	la		$a3, 64			#square size 
 	greenFillLoop:
 	jal		drawCircle		#Jump and link to drawCircle
 	addi		$a3, $a3, -1		#Decrement radius
 	bnez		$a3, greenFillLoop	#If a3 is not 0, branch
+	
+	#DRAW DIGIT
+	li		$a0, 187		#x 
+	li		$a1, 59			#y 
+	la		$a2, digit2
+	jal		OutText
 	
 	#PLAY TONE GREEN
 	li		$a0, 65			#Pitch
@@ -74,14 +119,20 @@ main:
 	syscall					#Execute
 	
 	#DRAW RED CIRCLE
-	la		$a0, 64			#x = 1
-	la		$a1, 192		#y = 1
-	la		$a2, 3			#colour = 1
-	la		$a3, 64			#square size = 14
+	la		$a0, 64			#x 
+	la		$a1, 192		#y
+	la		$a2, 3			#colour 
+	la		$a3, 64			#square size 
 	redFillLoop:
 	jal		drawCircle		#Jump and link to drawCircle
 	addi		$a3, $a3, -1		#Decrement radius
 	bnez		$a3, redFillLoop	#If a3 is not 0, branch
+	
+	#DRAW DIGIT
+	li		$a0, 59			#x 
+	li		$a1, 187		#y 
+	la		$a2, digit3
+	jal		OutText
 	
 	#PLAY TONE RED
 	li		$a0, 70			#Pitch
@@ -92,14 +143,20 @@ main:
 	syscall					#Execute
 	
 	#DRAW MAGENTA CIRCLE
-	la		$a0, 192		#x = 1
-	la		$a1, 192		#y = 1
-	la		$a2, 5			#colour = 1
-	la		$a3, 64			#square size = 14
+	la		$a0, 192		#x 
+	la		$a1, 192		#y 
+	la		$a2, 5			#colour  
+	la		$a3, 64			#square size 
 	magentaFillLoop:
 	jal		drawCircle		#Jump and link to drawCircle
 	addi		$a3, $a3, -1		#Decrement radius
 	bnez		$a3, magentaFillLoop	#If a3 is not 0, branch
+	
+	#DRAW DIGIT
+	li		$a0, 187		#x 
+	li		$a1, 187		#y 
+	la		$a2, digit4
+	jal		OutText
 	
 	#PLAY TONE MAGENTA
 	li		$a0, 75			#Pitch
@@ -1060,6 +1117,69 @@ drawCircle:
 	
 	jr		$ra
 	
+# OutText: display ascii characters on the bit mapped display
+# $a0 = horizontal pixel co-ordinate (0-255)
+# $a1 = vertical pixel co-ordinate (0-255)
+# $a2 = pointer to asciiz text (to be displayed)
+OutText:
+        addiu   $sp, $sp, -24
+        sw      $ra, 20($sp)
+
+        li      $t8, 1          # line number in the digit array (1-12)
+_text1:
+        la      $t9, 0x10040000 # get the memory start address
+        sll     $t0, $a0, 2     # assumes mars was configured as 256 x 256
+        addu    $t9, $t9, $t0   # and 1 pixel width, 1 pixel height
+        sll     $t0, $a1, 10    # (a0 * 4) + (a1 * 4 * 256)
+        addu    $t9, $t9, $t0   # t9 = memory address for this pixel
+
+        move    $t2, $a2        # t2 = pointer to the text string
+_text2:
+        lb      $t0, 0($t2)     # character to be displayed
+        addiu   $t2, $t2, 1     # last character is a null
+        beq     $t0, $zero, _text9
+
+        la      $t3, DigitTable # find the character in the table
+_text3:
+        lb      $t4, 0($t3)     # get an entry from the table
+        beq     $t4, $t0, _text4
+        beq     $t4, $zero, _text4
+        addiu   $t3, $t3, 13    # go to the next entry in the table
+        j       _text3
+_text4:
+        addu    $t3, $t3, $t8   # t8 is the line number
+        lb      $t4, 0($t3)     # bit map to be displayed
+
+        sw      $zero, 0($t9)   # first pixel is black
+        addiu   $t9, $t9, 4
+
+        li      $t5, 8          # 8 bits to go out
+_text5:
+        la      $t7, colourTable
+        lw      $t7, 0($t7)     # assume black
+        andi    $t6, $t4, 0x80  # mask out the bit (0=black, 1=white)
+        beq     $t6, $zero, _text6
+        la      $t7, colourTable     # else it is white
+        lw      $t7, 28($t7)
+_text6:
+        sw      $t7, 0($t9)     # write the pixel color
+        addiu   $t9, $t9, 4     # go to the next memory position
+        sll     $t4, $t4, 1     # and line number
+        addiu   $t5, $t5, -1    # and decrement down (8,7,...0)
+        bne     $t5, $zero, _text5
+
+        sw      $zero, 0($t9)   # last pixel is black
+        addiu   $t9, $t9, 4
+        j       _text2          # go get another character
+
+_text9:
+        addiu   $a1, $a1, 1     # advance to the next line
+        addiu   $t8, $t8, 1     # increment the digit array offset (1-12)
+        bne     $t8, 13, _text1
+
+        lw      $ra, 20($sp)
+        addiu   $sp, $sp, 24
+        jr      $ra
 
 	
 	
